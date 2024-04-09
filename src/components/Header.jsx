37c381105d1bba82b0a1a96eeeb23daba6c96208
21MoududAhammed <21moudud@gmail.com>
2024-04-09 16:48:12 +0600

    new task manager logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import logo from '../assets/lws-logo-en.svg'
+// import logo from '../assets/lws-logo-en.svg'
+import logo from '../assets/task-manager-logo.png'
 export default function Header() {
   return (
     <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-40 px-20">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <div className="flex items-center gap-2">
         <a href="/">
-          <img className="h-[45px]" src={logo} alt="Lws" />
+          <img className="h-[45px] rounded-full" src={logo} alt="Lws" />
         </a>
+        <h3 className='text-xl font-serif'>Task Manager</h3>
+        </div>
       </div>
     </nav>
   );
